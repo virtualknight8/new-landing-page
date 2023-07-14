8bd50ee439fcee451192b2849554314c4fd7a6ba
virtualknight8 <ankitkhola2@gmail.com>
2023-07-14 19:10:36 +0530

    made header mobile view

---
+++ src/app/components/Header.js
@@ -0,0 +1,17 @@
+
+import './Header.css'
+
+export default function Header(){
+    return(
+
+        <div className="px-5 py-5 flex-col header">
+            <p className="text-2xl font-medium mb-8 text-teal-900">ＭＵＬＴ♢Ｒ</p>
+            <p className="text-5xl font-bold text-teal-950 mb-8">Describe the value of booking an appointment</p>
+            <p className="tracking-wide">No need to get clever. Tell people exactly what you're offering,
+                 then use this space to communicate your key value proposition.</p>
+        </div>
+
+
+
+    )
+}
