7da2fb135d5f236986c088da354b1bab57c747b0
virtualknight8 <ankitkhola2@gmail.com>
2023-07-18 20:44:31 +0530

    changed width to max

--- src/app/components/Feature.js
+++ src/app/components/Feature.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Feature(props){
     return(
-        <div className="mb-5 px-3 flex flex-col mt-16" style={{width:'400px'}}>
-            <Image className="mb-6" src={props.imgPath} width={400} height={169} alt={'Feature images'} />
+        <div className="mb-5 px-3 flex flex-col mt-16" style={{maxWidth:'400px'}}>
+            <Image className="mb-6 max-w-sm" src={props.imgPath} width={400} height={169} alt={'Feature images'} />
             <p className="font-bold text-xl tracking-wider mb-3">{props.heading}</p>
             <p className="font-light w-4/5 tracking-wider">{props.description}</p>
         </div>
