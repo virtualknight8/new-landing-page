98d07f7df592935d583589cfb514f4faa926a281
virtualknight8 <ankitkhola2@gmail.com>
2023-07-22 00:16:17 +0530

    fixed feature wrap

--- src/app/components/Feature.js
+++ src/app/components/Feature.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Feature(props){
     return(
-        <div className="mb-5 px-3 flex flex-col mt-16">
+        <div className="mb-5 px-3 flex flex-col mt-16" style={{maxWidth:'400px'}}>
             <Image className="mb-6" src={props.imgPath} width={400} height={169} alt={'Feature images'} />
             <p className="font-bold text-xl tracking-wider mb-3">{props.heading}</p>
             <p className="font-light w-4/5 tracking-wider">{props.description}</p>
